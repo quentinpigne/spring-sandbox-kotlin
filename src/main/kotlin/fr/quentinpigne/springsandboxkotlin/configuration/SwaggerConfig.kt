69283f68ac6c892f50d971fa0b5e14afe8ab4539
Quentin Pigné <quentin.pigne@gmail.com>
2020-10-23 01:56:32 +0200

    adding Swagger

---
+++ src/main/kotlin/fr/quentinpigne/springsandboxkotlin/configuration/SwaggerConfig.kt
@@ -0,0 +1,23 @@
+package fr.quentinpigne.springsandboxkotlin.configuration
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.builders.RequestHandlerSelectors
+import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.Contact
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
+
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun api(): Docket = Docket(DocumentationType.SWAGGER_2).select().apis(RequestHandlerSelectors.any()).paths(PathSelectors
+            .any()).build().apiInfo(apiInfo())
+
+    private fun apiInfo(): ApiInfo {
+        return ApiInfo("My REST API", "Some custom description of API.", "API TOS", "Terms of service",
+                Contact("John Doe", "www.example.com", "myeaddress@company.com"), "License of API", "API license URL", emptyList())
+    }
+}
