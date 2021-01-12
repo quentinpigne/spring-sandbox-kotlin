5632bd5eb191d096d865052483f1f4b5b5f51c38
Quentin Pigné <quentin.pigne@gmail.com>
2021-01-12 18:46:46 +0100

    updating project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,11 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.3.4.RELEASE"
+	id("org.springframework.boot") version "2.4.1"
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
-	kotlin("jvm") version "1.3.72"
-	kotlin("plugin.spring") version "1.3.72"
-	kotlin("plugin.jpa") version "1.3.72"
+	kotlin("jvm") version "1.4.21"
+	kotlin("plugin.spring") version "1.4.21"
+	kotlin("plugin.jpa") version "1.4.21"
 }
 
 group = "fr.quentinpigne"
@@ -34,7 +34,7 @@ dependencies {
 	implementation("javax.cache:cache-api")
 	implementation("org.ehcache:ehcache-clustered")
 	implementation("io.springfox:springfox-boot-starter:3.0.0")
-	implementation("com.sndyuk:logback-more-appenders:1.8.1")
+	implementation("com.sndyuk:logback-more-appenders:1.8.4")
 	implementation("org.fluentd:fluent-logger:0.3.4")
 	implementation("org.komamitsu:fluency-core:2.4.1")
 	implementation("org.komamitsu:fluency-fluentd:2.4.1")
